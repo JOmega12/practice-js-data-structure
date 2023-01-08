457252ca633645972ab3a379dba7138e3f42d94f
Jensen Omega <omega90ej@gmail.com>
2023-01-08 00:55:30 -0800

    fixed e17 bug

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,67 +1,76 @@
 export function minBy(array, cb) {
-   /* const map = {};
-  for (element of array) {
-    const key = cb(element)
-    if(!(key in map)) {
-      map[key] = [element]
-      continue
+
+  
+  let firstArrayAge = array[0]
+  console.log(firstArrayAge, 'firstArrayAge')
+
+  if(!array || array.length=== 0) {
+    return undefined;
+  } else {
+    for(let i= 0; i< array.length; i++) {
+      //array[i].age is person => person.age
+      if(cb(firstArrayAge) > cb(array[i])) {
+        console.log(firstArrayAge, 'firstArrayAge');
+        console.log(cb(firstArrayAge), 'firstArrayAge cb');
+        console.log(array[i], 'array[i]');
+        // console.log(array[i].name, 'array[i].name')
+        firstArrayAge= array[i];
+      }
+      // console.log(array[i].age, 'array[i] age');
+      // console.log(cb, 'cb');
     }
-    map[key].push(element)
   }
-  return map */
 
-  const map = {};
-
-  for (element of array) {
-    
-    const key = cb(element)
-    if(!(key in map)) {
-      map[key] = [element]
-      continue
+  /* for(const item of array) {
+    console.log(cb(item), 'cb')
+    // console.log(item.age, 'item age');
+    if(!array) {
+      return undefined
     }
-    map[key].push(element)
-  }
-  /* console.log(map)
-  return map */
-
-   let minKey = Infinity;
-   let minElem;
-
-   for (key in map) {
-   if (key < minKey) {
-   minKey = key;
-   minElem = map[key][0];
-   }
-   }
-
-   return minElem;
+    else if(firstArrayAge > cb(item)) {
+      firstArrayAge = cb(item);
+      console.log(firstArrayAge, 'firstArrayAge IF');
+      console.log(cb(item),'item age cb');
+    }
+  } */
+  return firstArrayAge;
 
 }
 
 export function maxBy(array, cb) {
-   const map = {};
+  let firstArrayAge = array[0]
+  console.log(firstArrayAge, 'firstArrayAge')
 
-  for (element of array) {
-    
-    const key = cb(element)
-    if(!(key in map)) {
-      map[key] = [element]
-      continue
+/*   for(const item of array) {
+    console.log(cb(item), 'cb')
+    // console.log(item.age, 'item age');
+    if(!array) {
+      return undefined
     }
-    map[key].push(element)
+    else if(firstArrayAge < cb(item)) {
+      firstArrayAge = cb(item);
+      console.log(firstArrayAge, 'firstArrayAge IF');
+      console.log(cb(item),'item age cb');
+    }
+  } */
+
+  if(!array || array.length === 0) {
+    return undefined
+  } else {
+    for(let i= 0; i< array.length; i++) {
+      //array[i].age is person => person.age
+      if(cb(firstArrayAge) < cb(array[i])) {
+        console.log(firstArrayAge, 'firstArrayAge');
+        console.log(array[i], 'array[i]');
+        // console.log(array[i].name, 'array[i].name')
+        firstArrayAge= array[i];
+      }
+      console.log(array[i].age, 'array[i] age');
+      // console.log(cb, 'cb');
+      // const smallAge= array[i].age;
+      // console.log(smallAge, 'smallAge');
+    }
   }
-//   return map
 
-   let maxKey = -Infinity;
-   let maxElem = null;
-
-   for (key in map) {
-      if (key > maxKey) {
-         maxKey = key;
-         maxElem = map[key][0];
-         }
-   }
-   
-   return maxElem;
- 
+  return firstArrayAge;
 }
